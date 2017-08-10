b3d6b7b130c8f8e8a4b1d0f4a443542112349299
user <asd@mail.ru>
2017-08-10 18:34:30 +0500

    additional logic is added in mp4 packer to avoid bad H264 NAL units which cause hardware decoder to die in Chromee browser

--- libavformat/avc.c
+++ libavformat/avc.c
@@ -72,6 +72,7 @@ int ff_avc_parse_nal_units(AVIOContext *pb, const uint8_t *buf_in, int size)
     const uint8_t *p = buf_in;
     const uint8_t *end = p + size;
     const uint8_t *nal_start, *nal_end;
+    int forbidden_zero_bit;
 
     size = 0;
     nal_start = ff_avc_find_startcode(p, end);
@@ -81,11 +82,19 @@ int ff_avc_parse_nal_units(AVIOContext *pb, const uint8_t *buf_in, int size)
             break;
 
         nal_end = ff_avc_find_startcode(nal_start, end);
-        avio_wb32(pb, nal_end - nal_start);
-        avio_write(pb, nal_start, nal_end - nal_start);
-        size += 4 + nal_end - nal_start;
+
+	forbidden_zero_bit = *nal_start >> 7;
+
+	if(!forbidden_zero_bit)
+	{
+          avio_wb32(pb, nal_end - nal_start);
+          avio_write(pb, nal_start, nal_end - nal_start);
+          size += 4 + nal_end - nal_start;
+	}
+
         nal_start = nal_end;
     }
+
     return size;
 }
 
