af3c1a9b8d5e212e87ddb017c0aece766b581361
Bogdanov Kirill <kirill_art@live.ru>
2021-01-20 12:00:17 +0500

    test

--- libavformat/stream2shm.c
+++ libavformat/stream2shm.c
@@ -219,7 +219,7 @@ static int write_packet(AVFormatContext *s, AVPacket *pkt)
 
  time_base = &s->streams[pkt->stream_index]->time_base;
 
- cbd->timestamp = *s->timestamp_base + pkt->pts * time_base->num / time_base->den * 1000;
+ cbd->timestamp = av_rescale_q(pkt->pts, ist->time_base, (AVRational) { 1, 1000 });//pkt->pts * 1000 * time_base->num / time_base->den;
  cbd->width = width;
  cbd->height = height;
  cbd->bgr_stride = stride;
